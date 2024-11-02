5fdf3b2c147da9cec2d032fd4daee219ffc7d53e
junaralinsub <junaralinsub@gmail.com>
2024-11-02 13:59:03 +0800

    :sparkles: Implement POST request

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=["GET", "POST"])
 def login():
+  if request.method == "POST":
+    email = request.form.get("email")
+    password = request.form.get("password")
+    pass
+    
   return render_template("login.html", text="Loading...")
 
 @auth.route('/logout')
 def logout():
   return "<p>Logout</p>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods=["GET", "POST"])
 def signup():
+  if request.method == "POST":
+    email = request.form.get("email")
+    name = request.form.get("name")
+    password = request.form.get("password")
+    cpassword = request.form.get("cpassword")
+    
+    if password != cpassword:
+      flash("Password doesn't match", category="error")
+    else:
+      flash("Account created!", category="success")  
+    
+    
+  
   return render_template("signup.html")
