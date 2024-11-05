38ebdcb754795f65e274619130597b960475301d
junaralinsub <junaralinsub@gmail.com>
2024-11-05 11:22:18 +0800

    :sparkles: Connect database to the forms

--- website/auth.py
+++ website/auth.py
@@ -1,37 +1,68 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 import re
 
-auth = Blueprint('auth', __name__)
+from . import models
+from . import db
 
-@auth.route('/login', methods=["GET", "POST"])
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-  if request.method == "POST":
-    email = request.form.get("email")
-    password = request.form.get("password")
-    pass
-    
-  return render_template("login.html", text="Loading...")
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = models.User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully", category="success")
+            else:
+                # don't let the user know between account not existing and wrong email/password
+                flash("Wrong email / password", category="error")
+        else:
+            # don't let the user know between account not existing and wrong email/password
+            flash("Wrong email / password", category="error")
+        pass
+    return render_template("login.html")
 
-@auth.route('/logout')
+
+@auth.route("/logout")
 def logout():
-  return "<p>Logout</p>"
+    return "<p>Logout</p>"
 
-@auth.route('/signup', methods=["GET", "POST"])
+
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
-  if request.method == "POST":
-    email = request.form.get("email")
-    name = request.form.get("name")
-    password = request.form.get("password")
-    cpassword = request.form.get("cpassword")
-    if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
-      flash("Invalid email", category="error")
-    elif len(name) < 3:
-      flash("Name should be at least 4 characters", category="error")
-    elif len(password) < 7:
-      flash("Password should be at least 8 characters", category="error")
-    elif password != cpassword:
-      flash("Password doesn't match", category="error")
-    else:
-      flash("Account created!", category="success")  
+    if request.method == "POST":
+        email = request.form.get("email")
+        name = request.form.get("name")
+        password = request.form.get("password")
+        cpassword = request.form.get("cpassword")
+        user = models.User.query.filter_by(email=email).first()
 
-  return render_template("signup.html")
+        if user:
+            flash(
+                "It seems you already created an account. Try with another email.",
+                category="error",
+            )
+        elif not re.match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", email):
+            flash("Invalid email", category="error")
+        elif len(name) < 3:
+            flash("Name should be at least 4 characters", category="error")
+        elif len(password) < 7:
+            flash("Password should be at least 8 characters", category="error")
+        elif password != cpassword:
+            flash("Password doesn't match", category="error")
+        else:
+            new_user = models.User(
+                email=email,
+                full_name=name,
+                password=generate_password_hash(password, method="pbkdf2:sha256"),
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account created!", category="success")
+            return redirect(url_for("views.home"))
+
+    return render_template("signup.html")
